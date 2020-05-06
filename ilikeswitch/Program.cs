ae6faf21e00de0f7013c13f33b93b0795384a8ca
Kim Suhyeon <s2018s26@e-mirim.hs.kr>
2020-05-06 16:06:32 +0900

    7번 문제 풀이

--- ilikeswitch/Program.cs
+++ ilikeswitch/Program.cs
@@ -10,6 +10,27 @@ namespace ilikeswitch
     {
         static void Main(string[] args)
         {
+            Console.Write("학년을 입력하세요 : ");
+            int level = int.Parse(Console.ReadLine());
+
+            switch (level)
+            {
+                case 1:
+                    Console.WriteLine("수강해야 하는 전공 학점 : 12학점");
+                    break;
+                case 2:
+                    Console.WriteLine("수강해야 하는 전공 학점 : 18학점");
+                    break;
+                case 3:
+                    Console.WriteLine("수강해야 하는 전공 학점 : 10학점");
+                    break;
+                case 4:
+                    Console.WriteLine("수강해야 하는 전공 학점 : 18학점");
+                    break;
+                default:
+                    Console.WriteLine("졸업하세영");
+                    break;
+            }
         }
     }
 }
